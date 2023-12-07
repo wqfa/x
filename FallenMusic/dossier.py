b8fb7f7bd73a90ac9c0ea7331ad3cdb4b31dcb48
XF <118357460+wqfa@users.noreply.github.com>
2023-12-07 12:45:39 +0300

    Create dossier.py

---
+++ FallenMusic/dossier.py
@@ -0,0 +1,86 @@
+# MIT License
+#
+# Copyright (c) 2023 AnonymousX1025
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from FallenMusic import BOT_NAME
+
+PM_START_TEXT = """
+‌‌‏≪⊶⌯━‌‌‏♢ ⦓ ᥉᥆υᖇᥴᥱ ᥉ρᎥժᥱᖇ ⦔ ♢━‌‌‏⌯⊷≫
+
+⎊ اهــلا بـك » {0}
+
+⎊ انـا بــوت » {1}
+
+⎊ اســتـطـيـع تـشـغـيـل الـقـرآن الـكـريـم
+
+⎊ فــي الــــــمــــكـــالــمــات الــصــوتــيـــة
+
+⎊ تـابـع الازرار بـالاسفل لـمعرفة الاوامـࢪ
+
+‌‌‏≪⊶⌯━‌‌‏♢ ⦓ ᥉᥆υᖇᥴᥱ ᥉ρᎥժᥱᖇ ⦔ ♢━‌‌‏⌯⊷≫
+"""
+
+START_TEXT = """
+━‌‌‏─━─『𝗦𝗣𝗶𝗗𝗘𝗥』─━─━‌‌‏
+⎊ مـرحـبـا » {0} 
+⎊ انا بـوت استطيع تـشـغيـل
+⎊الـقـرآن الـكـريـم في الـكـول
+⎊ لـمـعـرفة اوامـر الـتـشـغـيـل
+⎊ ادخـــــــل الـــــى البــــــوت
+━‌‌‏─━─『𝗦𝗣𝗶𝗗𝗘𝗥』─━─━‌‌‏
+"""
+
+HELP_TEXT = f"""
+<u> **BoT Commands {BOT_NAME} :**</u>
+
+- To PlaY a SonG iN ThE VoiCe CalL
+TyPe تشغيل + اسم الاغنية
+
+- To temporarily StoP PlAyBaCk iN ThE Group
+TyPe كتم oR اسكت
+
+- To ReSume PlAyBaCk iN ThE CalL
+Type كمل oR الغاء الكتم 
+
+- SkiP anD PlaY ThE Next SonG 
+ TyPe تخطي oR التالي
+ 
+- To StoP PlAyBaCk iN ThE Group
+ Type ايقاف oR قف
+"""
+
+HELP_SUDO = f"""
+<u> **•Developer Orders• {BOT_NAME} :**</u>
+
+- TyPe المكالمات tO Know ThE CallS iN ThE BoT
+- TyPe /eval tO RuN oN ThE BoT TeRmiNaL
+- WriTe a فحص tO TeSt ThE SeRvEr SpeeD  
+- Type الاحصائيات tO ViEw SeRvRr STaTiStiCs
+"""
+
+HELP_DEV = f"""
+<u> **•Owner's Orders• {BOT_NAME} :**</u>
+
+- TyPe الفارات tO ReTriEvE ThE BoT pAramEteRs
+- WriTe a اذاعة anD MaKe a Report oN ThE RaDio
+- TyPe تنظيف tO ClEaR ThE BoT's CaChE
+- Type مغادة tO LEaVe ThE aSSisTant FoR ThE Groups
+"""
